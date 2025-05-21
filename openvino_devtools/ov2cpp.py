4aceef6a34c08f8e1e312df8fc08fc9867b9a3b4
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-05-21 15:49:15 +0400

    add ov2cpp.py

---
+++ openvino_devtools/ov2cpp.py
@@ -0,0 +1,16 @@
+import openvino as ov
+from openvino_devtools import builder
+import sys
+
+def ov2py(model, with_node_names=False, path_to_source_model=None, entry_point=False, with_tensor_names=True):
+    printer = builder.CppModelPrinter()
+    return printer.implement_model(model, with_node_names=with_node_names, path_to_source_model=path_to_source_model, entry_point=entry_point, with_tensor_names=with_tensor_names)
+
+def main():
+    model_path = sys.argv[1]
+    core = ov.Core()
+    model = core.read_model(model_path)
+    print(ov2py(model, with_node_names=True, path_to_source_model=model_path, entry_point=True, with_tensor_names=True), '\n')
+
+if __name__ == "__main__":
+    main()
